8d929e9ac442e02cae23cbe4dac5ab4bb9dba1f0
Enrico Borba <enricozb@gmail.com>
2020-09-02 08:31:39 -0700

    google storage credentials

--- server/main.py
+++ server/main.py
@@ -1,6 +1,10 @@
+import json
+import os
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from google.cloud import storage
+from google.oauth2 import service_account
 
 app = FastAPI()
 
@@ -12,12 +16,20 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-bucket = storage.Client().get_bucket("remarkable-templates")
+
+credentials = json.loads(os.environ.get("GOOGLE_CREDENTIALS"))
+
+credentials["private_key"] = credentials["private_key"].replace("\\n", "\n")
+
+bucket = storage.Client(
+    project="Remarkable Templates",
+    credentials=service_account.Credentials.from_service_account_info(credentials),
+).get_bucket("remarkable-templates")
 
 
 @app.get("/files")
 def files():
-    for f in bucket.list_blobs(prefix="templates"):
-        print('blob', f)
+    for blob in bucket.list_blobs(prefix="jsons"):
+        print("blob", f)
 
     return {}
